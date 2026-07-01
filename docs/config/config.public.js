e3cc09bbb58febf3b63ae7bff3329ae2256e425a
Layla <298504290+layla-000@users.noreply.github.com>
2026-07-01 19:49:25 +0900

    Connect Atlas Backend Endpoint

--- docs/config/config.public.js
+++ docs/config/config.public.js
@@ -1,1 +1,4 @@
 window.AtlasConfig.maps.apiKey = "AIzaSyBCCIW8JsILMyOOgCfzeurPkvqvHp8z2yY";
+backend: {
+    uploadEndpoint: "https://script.google.com/macros/s/AKfycbyC-kDrY0tgvLgZSvhryUHQy1ZPr11ySnH40ilJGsSJCn1fZb5YxPiFIW0ym3ElisUZGQ/exec"
+}
