32bba9598f1102a6b58ccb6f951e58b528971e46
Layla <298504290+layla-000@users.noreply.github.com>
2026-07-03 07:55:40 +0900

    Use image cards for quick actions

--- docs/js/atlas.app.js
+++ docs/js/atlas.app.js
@@ -184,7 +184,7 @@ function renderActions(links) {
             label: "Boarding Pass",
             sublabel: "탑승권 확인",
             url: links.boarding_pass,
-            imageSrc: "docs/assets/images/quick-actions/boardingpass.png",
+            imageSrc: "docs/assets/images/quick-actions/bp.png",
             imageAlt: "Boarding Pass"
           })}
 
