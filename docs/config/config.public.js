db4f793673923641f5688521f7f0d6a0c7afbe28
Layla <298504290+layla-000@users.noreply.github.com>
2026-07-03 21:25:08 +0900

    UP

--- docs/config/config.public.js
+++ docs/config/config.public.js
@@ -1,4 +1,5 @@
 window.AtlasConfig.maps.apiKey = "AIzaSyBCCIW8JsILMyOOgCfzeurPkvqvHp8z2yY";
-
-window.AtlasConfig.backend.uploadEndpoint =
-    "https://script.google.com/macros/s/AKfycbyC-kDrY0tgvLgZSvhryUHQy1ZPr11ySnH40ilJGsSJCn1fZb5YxPiFIW0ym3ElisUZGQ/exec";
+const endpoint =
+  window.ATLAS_WEB_APP_URL ||
+  window.AtlasConfig.backend.uploadEndpoint ||
+"https://script.google.com/macros/s/AKfycbyC-kDrY0tgvLgZSvhryUHQy1ZPr11ySnH40ilJGsSJCn1fZb5YxPiFIW0ym3ElisUZGQ/exec";
